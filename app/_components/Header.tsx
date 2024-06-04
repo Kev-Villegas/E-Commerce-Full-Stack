9cc085edd47e6d9c5a439d08d1a8d122aa924b0a
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-04 11:03:42 -0300

    feat: add initial header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { MenuIcon, Store } from "lucide-react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-between px-5 pt-6">
+      <Link href="/">
+        <div className="relative h-[30px] w-[100px]">
+          <Store />
+        </div>
+      </Link>
+      <nav>
+        <Button
+          size="icon"
+          variant="outline"
+          className="border-none bg-transparent"
+        >
+          <MenuIcon />
+        </Button>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main>{children}</main>
         <Toaster position="bottom-center" reverseOrder={false} />
       </body>
