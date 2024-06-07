826050deda3de0ec52f051928fe1242b4b7b4fd7
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-07 16:38:20 -0300

    feat: add product edit page with form and product retrieval

---
+++ app/products/[id]/edit/page.tsx
@@ -0,0 +1,24 @@
+import { db } from "@/app/_lib/prisma";
+import ProductForm from "../../_components/ProductForm";
+import { notFound } from "next/navigation";
+
+interface ProductEditProps {
+  params: {
+    id: string;
+  };
+}
+
+const EditProductPage = async ({ params }: ProductEditProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+  return <ProductForm product={product} />;
+};
+
+export default EditProductPage;
