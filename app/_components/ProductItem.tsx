65747ec5143b5e009e1e6d93cee80a86446fd3cb
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-07 10:37:59 -0300

    feat: build the product item component and installing card from shadcn-ui

---
+++ app/_components/ProductItem.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { cn } from "../_lib/utils";
+import { Product } from "@prisma/client";
+import { Button } from "../_components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/_components/ui/card";
+
+interface ProductItemProps {
+  product: Product;
+  className?: string;
+}
+
+const ProductItem = ({ product, className }: ProductItemProps) => {
+  return (
+    <Card className={cn("", className)}>
+      <CardHeader>
+        <CardTitle>{product.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="h-[90px]">
+        <p>{product.description}</p>
+      </CardContent>
+      <CardFooter className="flex items-center justify-between">
+        <h3 className="text-base font-medium">${product.price}</h3>
+        <Button>
+          <Link href={`/products/${product.id}`}>View Product</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ProductItem;
