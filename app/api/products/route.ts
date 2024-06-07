299fc965061ba168ad4853eea075b3cedabe7b5f
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-07 16:37:06 -0300

    refactor: move product schema to separate file and update route logic

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,48 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import { db } from "@/app/_lib/prisma";
-
-const createProductSchema = z.object({
-  name: z
-    .string()
-    .min(4, { message: "Product Name must be at least 4 characters!" })
-    .max(255),
-  description: z
-    .string()
-    .min(6, { message: "Product Description must be at least 6 characters!" })
-    .max(255)
-    .optional(),
-  price: z
-    .number()
-    .min(2, { message: "The minimum price required to upload a product is 2" })
-    .max(255),
-});
+import { productSchema } from "@/app/_utils/validationSchemas";
 
 export async function POST(request: NextRequest) {
-  try {
-    const body = await request.json();
-    const parsedBody = {
-      ...body,
-      price: Number(body.price.trim()),
-    };
+  const body = await request.json();
+  const validation = productSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
-    const validation = createProductSchema.safeParse(parsedBody);
-    if (!validation.success)
-      return NextResponse.json(validation.error.errors, { status: 400 });
+  const newProduct = await db.product.create({
+    data: { name: body.name, description: body.description, price: body.price },
+  });
 
-    const newProduct = await db.product.create({
-      data: {
-        name: parsedBody.name,
-        description: parsedBody.description,
-        price: parsedBody.price,
-      },
-    });
-
-    return NextResponse.json(newProduct, { status: 201 });
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 },
-    );
-  }
+  return NextResponse.json(newProduct, { status: 201 });
 }
