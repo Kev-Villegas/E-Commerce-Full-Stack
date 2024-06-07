cc910b2e28651a4f6b37dc6b111475181ef9a6d1
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-07 18:26:15 -0300

    feat: adding search component to products panel and adding button to handle the route to the panel

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { db } from "@/app/_lib/prisma";
+import Link from "next/link";
 import {
   Table,
   TableBody,
@@ -9,17 +10,16 @@ import {
   TableHeader,
   TableRow,
 } from "@/app/_components/ui/table";
-import { Button } from "@/app/_components/ui/button";
 import { Pencil } from "lucide-react";
-
+import Search from "@/app/_components/Search";
+import { Button } from "@/app/_components/ui/button";
 import DeleteDialog from "@/app/_components/DeleteDialog";
-import Link from "next/link";
-// import DeleteDialog from "@/app/_components/DeleteDialog";
 
 const ProductsPage = async () => {
   const products = await db.product.findMany({});
   return (
     <div className="p-5">
+      <Search />
       <div className="mb-4 flex justify-end px-2">
         <Button className="">
           <Link href="/products/new">New Product</Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import Search from "./_components/Search";
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-    <main className="space-y-3 p-5">
+    <main className="space-y-3 px-5">
       <Search />
       <div>Hello World</div>
+      <Button className="px-5">
+        <Link href="/admin/products">Manage Products</Link>
+      </Button>
     </main>
   );
 }
