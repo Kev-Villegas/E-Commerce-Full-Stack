894664990bd4bab8c42c2e1f8c4d4329fd2d7bcc
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-07 18:09:13 -0300

    style: adding some padding and margin

--- app/_components/Search.tsx
+++ app/_components/Search.tsx
@@ -25,7 +25,7 @@ const Search = () => {
   };
 
   return (
-    <form className="mb-6 flex gap-2" onSubmit={handleSearchSubmit}>
+    <form className="mb-4 mt-6 flex gap-2 px-5" onSubmit={handleSearchSubmit}>
       <Input
         placeholder="Find Products..."
         className="border-[1px] border-zinc-700 bg-zinc-300"
