0c19089ac48520d49f8879f0a5b402ca6c998a68
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-10 11:52:58 -0300

    feat: add GET endpoint for fetching products in /api/products

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,3 +1,4 @@
+/*eslint-disable no-unused-vars*/
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/app/_lib/prisma";
 import { productSchema } from "@/app/_utils/validationSchemas";
@@ -13,4 +14,17 @@ export async function POST(request: NextRequest) {
   });
 
   return NextResponse.json(newProduct, { status: 201 });
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    const products = await db.product.findMany();
+    return NextResponse.json(products, { status: 200 });
+  } catch (error) {
+    console.error("Failed to fetch products", error);
+    return NextResponse.json(
+      { error: "Failed to fetch products" },
+      { status: 500 },
+    );
+  }
 }
