af524b84657d04830f4da5a14a107975f9eafea2
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-10 17:17:23 -0300

    feat: update the values of the productSchema and error messages

--- app/_utils/validationSchemas.ts
+++ app/_utils/validationSchemas.ts
@@ -3,17 +3,19 @@ import { z } from "zod";
 export const productSchema = z.object({
   name: z
     .string()
-    .min(4, { message: "Product Name must be at least 4 characters!" })
-    .max(255),
-  description: z.optional(
-    z
-      .string()
-      .min(4, { message: "Description must be at least 4 characters!" })
-      .max(255),
-  ),
+    .min(3, { message: "Product Name must be at least 3 characters!" })
+    .max(255, { message: "Product Name must be at most 255 characters!" }),
+  description: z
+    .string()
+    .min(12, {
+      message: "Description must be at least 12 characters!",
+    })
+    .max(255, {
+      message: "Description must be at most 255 characters!",
+    }),
   price: z
-    .number()
+    .number({ message: "Price must be a number!" })
     .positive({ message: "Price must be a positive number!" })
-    .min(2, { message: "The minimum price required to upload a product is 2" })
-    .max(255),
+    .min(6, { message: "The minimum price required to upload a product is 6" })
+    .max(999, { message: "The maximum price allowed is 999" }),
 });
