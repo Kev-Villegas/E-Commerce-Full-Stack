cc3adb257944011c0b3ff0eeb35b610ee9ac58b5
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-10 15:38:58 -0300

    feat: use axios from fetching the products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
-import { db } from "@/app/_lib/prisma";
+"use client";
+
+import axios from "axios";
 import Link from "next/link";
+import { Pencil } from "lucide-react";
+import { Product } from "@prisma/client";
+import Search from "@/app/_components/Search";
+import Spinner from "@/app/_components/Spinner";
+import React, { useState, useEffect } from "react";
+import { Button } from "@/app/_components/ui/button";
+import DeleteDialog from "@/app/_components/DeleteDialog";
 import {
   Table,
   TableBody,
@@ -10,13 +18,26 @@ import {
   TableHeader,
   TableRow,
 } from "@/app/_components/ui/table";
-import { Pencil } from "lucide-react";
-import Search from "@/app/_components/Search";
-import { Button } from "@/app/_components/ui/button";
-import DeleteDialog from "@/app/_components/DeleteDialog";
 
-const ProductsPage = async () => {
-  const products = await db.product.findMany({});
+const ProductsPage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("/api/products");
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Failed to fetch products from the database", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="p-5">
       <Search />
@@ -25,42 +46,46 @@ const ProductsPage = async () => {
           <Link href="/products/new">New Product</Link>
         </Button>
       </div>
-      <Table>
-        <TableCaption>Fenix Products</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead></TableHead>
-            <TableHead></TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {products.map((product) => (
-            <TableRow key={product.id}>
-              <TableCell className="font-medium">{product.id}</TableCell>
-              <TableCell>{product.name}</TableCell>
-              <TableCell>{product.description}</TableCell>
-              <TableCell>${product.price.toFixed(2)}</TableCell>
-              <TableCell>
-                <Button className="gap-2">
-                  <Link href={`/products/${product.id}/edit`}>
-                    <div className="flex items-center">
-                      <Pencil size={17} className="mr-2 text-slate-500" />
-                      <span>Edit</span>
-                    </div>
-                  </Link>
-                </Button>
-              </TableCell>
-              <TableCell>
-                <DeleteDialog productId={product.id} />
-              </TableCell>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Table>
+          <TableCaption>Fenix Products</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">ID</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead></TableHead>
+              <TableHead></TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell className="font-medium">{product.id}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{product.description}</TableCell>
+                <TableCell>${product.price.toFixed(2)}</TableCell>
+                <TableCell>
+                  <Button className="gap-2">
+                    <Link href={`/products/${product.id}/edit`}>
+                      <div className="flex items-center">
+                        <Pencil size={17} className="mr-2 text-slate-500" />
+                        <span>Edit</span>
+                      </div>
+                    </Link>
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <DeleteDialog productId={product.id} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </div>
   );
 };
