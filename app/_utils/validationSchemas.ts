d5f0c4f2b6d97f4e374cf99eaca20bdddf2a0395
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-11 09:12:14 -0300

    feat: adding the clientSchema validation using zod

--- app/_utils/validationSchemas.ts
+++ app/_utils/validationSchemas.ts
@@ -19,3 +19,41 @@ export const productSchema = z.object({
     .min(6, { message: "The minimum price required to upload a product is 6" })
     .max(999, { message: "The maximum price allowed is 999" }),
 });
+
+export const clientSchema = z.object({
+  firstName: z
+    .string()
+    .min(3, { message: "First Name must be at least 3 characters!" })
+    .max(255, { message: "First Name must be at most 255 characters!" }),
+  lastName: z
+    .string()
+    .min(3, { message: "Last Name must be at least 3 characters!" })
+    .max(255, { message: "Last Name must be at most 255 characters!" }),
+  email: z
+    .string()
+    .email({ message: "Email must be a valid email address!" })
+    .min(3, { message: "Email must be at least 3 characters!" })
+    .max(255, { message: "Email must be at most 255 characters!" }),
+  phone: z
+    .string()
+    .min(6, { message: "Phone must be at least 6 characters!" })
+    .max(20, { message: "Phone must be at most 20 characters!" }),
+  address: z
+    .string()
+    .min(4, { message: "Address must be at least 4 characters!" })
+    .max(255, { message: "Address must be at most 255 characters!" }),
+  dni: z
+    .number({ message: "DNI must be a number!" })
+    .positive({
+      message: "DNI must be a positive number!",
+    })
+    .min(7, { message: "The minimum DNI of a client is 7" })
+    .max(8, { message: "The maximum DNI allowed is 8" }),
+  cuil: z
+    .number({ message: "CUIL must be a number!" })
+    .positive({
+      message: "CUIL must be a positive number!",
+    })
+    .min(11, { message: "The minimum CUIL of a client is 11" })
+    .max(11, { message: "The maximum CUIL allowed is 11" }),
+});
