687091b4920d277164d125e234840af30c04651b
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-12 19:52:02 -0300

    feat: add PATCH endpoint for updating clients

---
+++ app/api/clients/[id]/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/app/_lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { clientSchema } from "@/app/_utils/validationSchemas";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const body = await request.json();
+  const validation = clientSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const client = await db.client.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!client)
+    return NextResponse.json({ error: "Client not found" }, { status: 404 });
+
+  const updatedClient = await db.client.update({
+    where: { id: client.id },
+    data: {
+      firstName: body.firstName,
+      lastName: body.lastName,
+      email: body.email,
+      phone: body.phone,
+      address: body.address,
+      dni: body.dni,
+      cuil: body.cuil,
+    },
+  });
+
+  return NextResponse.json(updatedClient, { status: 200 });
+}
