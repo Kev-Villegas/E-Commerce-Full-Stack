550e12d9a13ea071f383e90d82fe2df0bab06330
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-14 11:36:12 -0300

    feat: adding header sheet functionality with navigation links

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,24 @@
 "use client";
+
 import Link from "next/link";
+import { Avatar } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { MenuIcon, Store } from "lucide-react";
+import { Separator } from "./ui/separator";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import {
+  HomeIcon,
+  MenuIcon,
+  PackageSearch,
+  Store,
+  Bitcoin,
+  UserRoundSearch,
+} from "lucide-react";
 
 const Header = () => {
   return (
@@ -11,15 +28,75 @@ const Header = () => {
           <Store />
         </div>
       </Link>
-      <nav>
-        <Button
-          size="icon"
-          variant="outline"
-          className="border-none bg-transparent"
-        >
-          <MenuIcon />
-        </Button>
-      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            size="icon"
+            variant="outline"
+            className="border-none bg-transparent"
+          >
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="rounded-bl-3xl rounded-tl-3xl">
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+          <>
+            <div className="flex justify-between pt-6">
+              <div className="flex items-center justify-center text-start">
+                <Avatar className="ml-2 items-center">
+                  <Bitcoin />
+                </Avatar>
+                <div>
+                  <h3 className="font-semibold">Cardano Enjoyer</h3>
+                  <span className="block text-xs text-muted-foreground">
+                    Cardano@example.com
+                  </span>
+                </div>
+              </div>
+            </div>
+          </>
+          <div className="py-4">
+            <Separator />
+          </div>
+          <div className="space-y-2">
+            <Button
+              variant="ghost"
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              asChild
+            >
+              <Link href="/">
+                <HomeIcon size={16} />
+                <span className="block">Home</span>
+              </Link>
+            </Button>
+            <>
+              <Button
+                variant="ghost"
+                className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+                asChild
+              >
+                <Link href="/admin/products">
+                  <PackageSearch size={18} />
+                  <span className="block">Manage Products</span>
+                </Link>
+              </Button>
+
+              <Button
+                variant="ghost"
+                className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+                asChild
+              >
+                <Link href="/admin/clients">
+                  <UserRoundSearch size={18} />
+                  <span className="block">Manage Clients</span>
+                </Link>
+              </Button>
+            </>
+          </div>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 };
