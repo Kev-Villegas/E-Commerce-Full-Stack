b51acf7134f0b8a36a7f6c1434510358a2f582e8
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-06-14 14:28:35 -0300

    feat: improve clientSchema validations

--- app/_utils/validationSchemas.ts
+++ app/_utils/validationSchemas.ts
@@ -22,27 +22,64 @@ export const productSchema = z.object({
 
 export const clientSchema = z.object({
   firstName: z
-    .string()
+    .string({
+      required_error: "First Name is required!",
+      invalid_type_error: "First Name must be a string!",
+    })
     .min(3, { message: "First Name must be at least 3 characters!" })
     .max(255, { message: "First Name must be at most 255 characters!" }),
   lastName: z
-    .string()
+    .string({
+      required_error: "Last Name is required!",
+      invalid_type_error: "Last Name must be a string!",
+    })
     .min(3, { message: "Last Name must be at least 3 characters!" })
     .max(255, { message: "Last Name must be at most 255 characters!" }),
   email: z
-    .string()
+    .string({
+      required_error: "Email is required!",
+      invalid_type_error: "Email must be a string!",
+    })
     .email({ message: "Email must be a valid email address!" })
     .min(3, { message: "Email must be at least 3 characters!" })
     .max(255, { message: "Email must be at most 255 characters!" }),
-  phone: z.string().min(6, { message: "Phone must be at least 6 characters!" }),
+  phone: z
+    .string({
+      required_error: "Phone is required!",
+    })
+    .min(6, { message: "Phone must be at least 6 characters!" })
+    .max(15, { message: "Phone must be at most 15 characters!" })
+    .refine((value) => /^[+]{1}(?:[0-9-()/.]\s?){6,15}[0-9]{1}$/.test(value), {
+      message: "Phone must be a valid phone number!",
+    }),
   address: z
-    .string()
-    .min(4, { message: "Address must be at least 4 characters!" })
+    .string({
+      required_error: "Address is required!",
+      invalid_type_error: "Address must be a string!",
+    })
+    .min(6, { message: "Address must be at least 6 characters!" })
     .max(255, { message: "Address must be at most 255 characters!" }),
-  dni: z.number({ message: "DNI must be a number!" }).positive({
-    message: "DNI must be a positive number!",
-  }),
-  cuil: z.number({ message: "CUIL must be a number!" }).positive({
-    message: "CUIL must be a positive number!",
-  }),
+  dni: z
+    .number({
+      required_error: "DNI is required!",
+    })
+    .positive({ message: "DNI must be a positive number!" })
+    .int({ message: "DNI must be an integer!" })
+    .refine(
+      (value) => {
+        const length = value.toString().length;
+        return length >= 7 && length <= 8;
+      },
+      { message: "DNI must be between 7 and 8 digits!" },
+    ),
+  cuil: z
+    .number({
+      required_error: "CUIL is required!",
+      invalid_type_error: "CUIL must be a number!",
+    })
+    .positive({ message: "CUIL must be a positive number!" })
+    .int({ message: "CUIL must be an integer!" })
+    .refine((value) => value.toString().length === 11, {
+      message: "CUIL must be exactly 11 digits!",
+    }),
 });
