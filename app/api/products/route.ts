bfc99ca4a5f249f02089736f4b98b780f2f78fbf
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-07-01 15:25:29 -0300

    refactor: update POST function to handle image upload and product creation with UUID-based filenames

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,36 +3,49 @@ import path from "path";
 import { db } from "@/app/_lib/prisma";
 import { writeFile } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
-import { productSchema } from "@/app/_utils/validationSchemas";
+import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: any) {
   try {
     const data = await request.formData();
     const image = data.get("image");
+
+    if (!image) {
+      return NextResponse.json(
+        { error: "No image received." },
+        { status: 400 },
+      );
+    }
+
     const bytes = await image.arrayBuffer();
     const buffer = Buffer.from(bytes);
+
+    const originalFileName = image.name;
+    const extension = path.extname(originalFileName);
+    const uniqueFileName = `${uuidv4()}${Date.now()}${Math.random().toString(36).substring(2, 15)}${extension}`;
     const filePath = path.join(
       process.cwd(),
       "public",
       "upload",
       "products",
-      image.name,
+      uniqueFileName,
     );
     await writeFile(filePath, buffer);
+
     const price = parseFloat(data.get("price") as string);
     const newProduct = await db.product.create({
       data: {
         name: data.get("name") as string,
         description: data.get("description") as string,
         price: price,
-        imagePath: `/public/upload/products/${image.name}`,
+        imagePath: `/upload/products/${uniqueFileName}`,
       },
     });
     return NextResponse.json(newProduct, { status: 201 });
   } catch (error) {
-    console.error("Failed to create product", error);
+    console.error("Failed to create product :(", error);
     return NextResponse.json(
-      { error: "Failed to create product" },
+      { error: "Failed to create product :(" },
       { status: 500 },
     );
   }
