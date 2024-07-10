7aa7b833175d06ee4018f764c23b4f491eb6b639
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-07-10 15:02:14 -0300

    refactor: update product PATCH route to handle image uploads

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,32 +1,84 @@
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
 import { db } from "@/app/_lib/prisma";
+import { writeFile, unlink } from "fs/promises";
 import { NextRequest, NextResponse } from "next/server";
-import { productSchema } from "@/app/_utils/validationSchemas";
+// import { productSchema } from "@/app/_utils/validationSchemas";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } },
 ) {
-  const body = await request.json();
-  const validation = productSchema.safeParse(body);
-  if (!validation.success)
-    return NextResponse.json(validation.error.format(), { status: 400 });
+  try {
+    const formData = await request.formData();
+    const image = formData.get("image");
+    const name = formData.get("name") as string;
+    const description = formData.get("description") as string;
+    const price = parseFloat(formData.get("price") as string);
 
-  const product = await db.product.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-  if (!product)
-    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    // const validation = productSchema.safeParse({
+    //   image,
+    //   name,
+    //   description,
+    //   price,
+    // });
+    // if (!validation.success)
+    //   return NextResponse.json(validation.error.format(), { status: 400 });
 
-  const updatedProduct = await db.product.update({
-    where: { id: product.id },
-    data: {
-      name: body.name,
-      description: body.description,
-      price: body.price,
-    },
-  });
+    const product = await db.product.findUnique({
+      where: { id: parseInt(params.id) },
+    });
 
-  return NextResponse.json(updatedProduct, { status: 200 });
+    if (!product)
+      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+
+    let imagePath = product.imagePath;
+
+    if (image && (image as File).arrayBuffer) {
+      const imageFile = image as File;
+      const bytes = await imageFile.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      const originalFileName = imageFile.name;
+      const extension = path.extname(originalFileName);
+      const uniqueFileName = `${uuidv4()}${Date.now()}${Math.random().toString(36).substring(2, 15)}${extension}`;
+      const filePath = path.join(
+        process.cwd(),
+        "public",
+        "upload",
+        "products",
+        uniqueFileName,
+      );
+      await writeFile(filePath, buffer);
+
+      // Deleting the old image if it exists
+      if (imagePath) {
+        const oldImagePath = path.join(process.cwd(), "public", imagePath);
+        await unlink(oldImagePath).catch((err) =>
+          console.error("Failed to delete old image:", err),
+        );
+      }
+
+      imagePath = `/upload/products/${uniqueFileName}`;
+    }
+
+    const updatedProduct = await db.product.update({
+      where: { id: product.id },
+      data: {
+        name,
+        description,
+        price,
+        imagePath,
+      },
+    });
+
+    return NextResponse.json(updatedProduct, { status: 200 });
+  } catch (error) {
+    console.error("Failed to update product:", error);
+    return NextResponse.json(
+      { error: "Failed to update product" },
+      { status: 500 },
+    );
+  }
 }
 
 export async function DELETE(
