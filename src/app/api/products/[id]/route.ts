79e039b8dda835c0198581fc0dfcb684117c074b
Kevin Villegas <kevin.villegas1973@gmail.com>
2024-07-28 00:03:59 -0300

    feat: implementing cloud image upload in PATCH method

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,9 +1,14 @@
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "@/src/_lib/prisma";
-import { writeFile, unlink } from "fs/promises";
+import { v2 as cloudinary } from "cloudinary";
 import { NextRequest, NextResponse } from "next/server";
-// import { productSchema } from "@/app/_utils/validationSchemas";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export async function PATCH(
   request: NextRequest,
@@ -15,15 +20,6 @@ export async function PATCH(
     const name = formData.get("name") as string;
     const description = formData.get("description") as string;
     const price = parseFloat(formData.get("price") as string);
-
-    // const validation = productSchema.safeParse({
-    //   image,
-    //   name,
-    //   description,
-    //   price,
-    // });
-    // if (!validation.success)
-    //   return NextResponse.json(validation.error.format(), { status: 400 });
 
     const product = await db.product.findUnique({
       where: { id: parseInt(params.id) },
@@ -40,25 +36,43 @@ export async function PATCH(
       const buffer = Buffer.from(bytes);
       const originalFileName = imageFile.name;
       const extension = path.extname(originalFileName);
-      const uniqueFileName = `${uuidv4()}${Date.now()}${Math.random().toString(36).substring(2, 15)}${extension}`;
-      const filePath = path.join(
-        process.cwd(),
-        "public",
-        "upload",
-        "products",
-        uniqueFileName,
-      );
-      await writeFile(filePath, buffer);
+      const uniqueFileName = `${uuidv4()}${Date.now()}${Math.random()
+        .toString(36)
+        .substring(2, 15)}${extension}`;
 
-      // Deleting the old image if it exists
+      const imageResponse = await new Promise<{ secure_url: string }>(
+        (resolve, reject) => {
+          cloudinary.uploader
+            .upload_stream(
+              {
+                folder: "fenix-app-images",
+                filename_override: uniqueFileName,
+                unique_filename: true,
+                public_id: uniqueFileName,
+              },
+              (error, result) => {
+                if (result) {
+                  resolve(result);
+                } else {
+                  reject(new Error("Image upload failed"));
+                }
+              },
+            )
+            .end(buffer);
+        },
+      );
+
+      // !Delete the old image if it exists on Cloudinary
       if (imagePath) {
-        const oldImagePath = path.join(process.cwd(), "public", imagePath);
-        await unlink(oldImagePath).catch((err) =>
-          console.error("Failed to delete old image:", err),
-        );
+        const publicId = imagePath.split("/").slice(-1)[0].split(".")[0]; // Extract public ID
+        await cloudinary.uploader
+          .destroy(`fenix-app-images/${publicId}`)
+          .catch((err) =>
+            console.error("Failed to delete old image on Cloudinary:", err),
+          );
       }
 
-      imagePath = `/upload/products/${uniqueFileName}`;
+      imagePath = `${imageResponse.secure_url}`;
     }
 
     const updatedProduct = await db.product.update({
